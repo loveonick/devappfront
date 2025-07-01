baf2a2a5c2e011f43395e8e9e1ee44b62fe51621
yunb12345 <agusyoon11@hotmail.com>
2025-06-30 23:26:25 -0300

    integracion con el backend para el auth de login

---
+++ app/api/auth_api.tsx
@@ -0,0 +1,23 @@
+const url = 'http://localhost:8080/api';
+
+export const loginApi = async (email,password) => {
+    try{
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: JSON.stringify({ email, password }),
+        };
+
+        const res = await fetch(`${url}/login`, requestOptions);
+        if (!res.ok) throw new Error('Error al iniciar sesión');
+
+        const data = await res.json();
+        return data.user
+    } catch(error){
+        console.log(error)
+        throw error
+    }
+}
